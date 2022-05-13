1ca9c67b52bee0d85c174fcec3ee1c4376b7d5e1
Gautam Sinha <gautam.sinha@live.com>
2022-05-12 22:58:38 -0400

    char select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Player from "./components/Player";
 
-function App() {
+export default function App() {
+  const [characters, setCharacter] = useState([])
+  const [player, setSelectedPlayer] = useState([])
+  useEffect(() => {
+    (async () => {
+      let req = await fetch('http://localhost:9292/characters')
+      let res = await req.json() 
+      setCharacter(res)
+    })()
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> 
+      <h1>Welcome to Mario Tennis</h1>
+      <div className="game-table">
+      { player && <Player player={player} /> }
+      </div> 
+       <div className="player-container">
+     {
+       characters.map((e, i) => {
+         return(
+          <div key={i} className="player-card" onClick={() => {
+setSelectedPlayer([e.character, ...player].slice(0,2))
+        }}> 
+           <h1>{e.character}</h1>
+           </div> 
+           )
+       }) 
+     }
+     </div> 
+    // </div>
   );
 }
 
-export default App;
+// const Message = ({text}) => (<p>{text}</p>)
+
+// const App = () => {
+//   const [messageText, setMessageText] = useState(null);
+  
+//   return(
+//     <div className="box">
+//       {messageText && <Message text={messageText} /> }
+//       <button onClick={() => setMessageText("Hi")}>Click Me</button>
+//     </div>
+//   );
+// }
+
+
+// export default App; 
---
+++ src/components/Player.js
@@ -0,0 +1,12 @@
+import React from "react" 
+
+const Player = ({player}) => {
+    // console.log(player)
+    return (
+        <div className="player-lobby">
+         {player}
+        </div> 
+    )
+}
+
+export default Player;
